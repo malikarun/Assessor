5e62d826a816579c379555adcac55e55a5c9b2e0
Arun Malik <akmalik@ymail.com>
2014-06-08 11:21:44 +1000

    changes are good

---
+++ assessor/app/controllers/bests_controller.rb
@@ -0,0 +1,19 @@
+class BestsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ assessor/test/helpers/bests_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class BestsHelperTest < ActionView::TestCase
+end
